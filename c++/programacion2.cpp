998d0f4d400f179ce20c0b218447cafb6dfb6078
odaherrera <98915024+odaherrera@users.noreply.github.com>
2022-02-26 17:39:04 -0400

    Add files via upload

---
+++ c++/programacion2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number1;
+    int number2;
+
+    cout<< "Enter the first number : " ;
+    cin>>number1;
+
+     cout<< "Enter the second number : " ;
+    cin>>number2;
+
+    cout<< "Results : "<< (number1-number2)<< endl;
+
+
+
+ return 0;
+}
